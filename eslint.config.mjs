8ed2d2eeee7d143ffed8773c43ec1f32e99ee376
Armindster <44671283+Armindster@users.noreply.github.com>
2025-02-18 11:14:35 +0100

    base TS project

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import stylisticJs from '@stylistic/eslint-plugin'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    plugins: {
+      '@stylistic': stylisticJs,
+    },
+    rules: {
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/quotes': ['error', 'single'],
+    },
+  }
+];
+
+export default eslintConfig;
