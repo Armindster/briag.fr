22b4ca04306a447939fd6ad8d088ee3e2cc66dde
Armindster <44671283+Armindster@users.noreply.github.com>
2025-03-01 17:26:39 +0100

    add content

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,53 @@
-import Image from 'next/image';
 import styles from './page.module.css';
+import Card from '@/components/card';
+import Sidebar from '@/components/sidebar';
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      Hello world
+      <Sidebar />
+      <div style={{ flex: 1, display: 'grid', padding: '2rem', gap: '1rem',  overflowY: 'auto' }}>
+        <Card
+          title="KLAVA Innovation"
+          content={`KLAVA Innovation développe des solutions numériques pour aider les personnes
+souffrant d'addictions. Leur solution phare, Quitoxil, est un dispositif médical certifié CE sur
+smartphone qui accompagne les utilisateurs dans leur sevrage tabagique. Il propose des thérapies
+cognitivo-comportementales personnalisées, des articles rédigés par des professionnels de la santé
+pour mieux comprendre l'addiction au tabac et une assistance avec de patients-experts.`}
+          image="https://klava-innovation.com/wp-content/uploads/2021/02/KLAVA-INNOVATION-LOGO-2021-large-.png"
+          link="https://klava-innovation.com/"
+        />
+        <Card
+          title="KMEX Consulting"
+          content={`KMEX Consulting est une entreprise de service spécialisée dans la mise en place
+de solution de gestion pour les entreprises du Bâtiment et des Travaux Publics. KMEX est également
+éditeur de logiciels de Suivi budgétaire de chantiers.`}
+          image="https://www.kmex.fr/wp-content/uploads/2021/10/logo-kmex.png"
+          link="https://www.kmex.fr/"
+        />
+        <Card
+          title="EasyTerms"
+          content={`EasyTerms est une application mobile innovante qui permet de résumer rapidement
+les conditions générales d'utilisation (CGU) de différentes applications. En quelques clics, les
+utilisateurs peuvent accéder à une synthèse claire et concise des termes et conditions souvent
+longs et complexes que l'on trouve dans les CGU. EasyTerms est un outil pratique pour les
+utilisateurs qui souhaitent comprendre rapidement les engagements qu'ils prennent en utilisant une
+application donnée. Avec une base de données en constante évolution, EasyTerms est la solution
+idéale pour tous ceux qui cherchent à comprendre les CGU sans passer des heures à les lire en
+entier.`}
+          image="https://cdn.dorik.com/6401e90495c7c300a9cb4867/6401ea9995c7c300a9cb487f/images/easy_terms_clear_8091nr9u.png"
+          link="https://easyterms.dorik.io/"
+        />
+        <Card
+          title="Jhonny and Jhon"
+          content={`Vous incarnez Jhonny et Jhon. Jhonny est aveugle et Jhon est en fauteuil
+roulant. Votre objectif est de faire sortir Jhonny et Jhon du bâtiment aussi vite que possible en
+utilisant leurs deux capacités. Vous devrez collecter des objets et des clés pour déverrouiller les
+portes afin de vous échapper.`}
+          image="https://img.itch.zone/aW1nLzE5NTI4NjUucG5n/315x250%23c/APAiaL.png"
+          link="https://superzikoure.itch.io/jhonny-and-jhon"
+        />
+      </div>
     </div>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.itch.zone',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.dorik.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.kmex.fr',
+      },
+      {
+        protocol: 'https',
+        hostname: 'klava-innovation.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
